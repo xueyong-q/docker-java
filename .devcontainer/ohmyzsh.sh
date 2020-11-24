a5e846a01d4cf2fde6652ee64d3a4c512ccb4df2
Yong <freeyongx@163.com>
2020-11-24 16:20:11 +0800

    update ohmyzsh script

--- .devcontainer/ohmyzsh.sh
+++ .devcontainer/ohmyzsh.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && \
-cat ~/.oh-my-zsh/templates/zshrc.zsh-template >> ~/.zshrc
+if [ ! -e ~/ohmyzsh.lock ]; then
+    
+    if [ -e ~/.oh-my-zsh ]; then rm -rf ~/.oh-my-zsh; fi
+    
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && \
+    cat ~/.oh-my-zsh/templates/zshrc.zsh-template >> ~/.zshrc && \
+    echo "1" > ~/ohmyzsh.lock
+fi
