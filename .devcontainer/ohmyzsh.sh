3e06e972500349a8037de01b485809566043875a
Yong <freeyongx@163.com>
2020-11-24 07:50:05 +0000

    update

--- .devcontainer/ohmyzsh.sh
+++ .devcontainer/ohmyzsh.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && \
 cat ~/.oh-my-zsh/templates/zshrc.zsh-template >> ~/.zshrc
